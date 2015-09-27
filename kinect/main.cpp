3ea976d2885b0bf2b83fe18449c1b5859f702e1e
Bruno Vieira Costa <brunopsycon@gmail.com>
2015-09-27 19:06:25 -0300

    separado o myFileLogger em outro arquivo

--- kinect/main.cpp
+++ kinect/main.cpp
@@ -27,22 +27,7 @@
 using namespace cv;
 using namespace std;
 
-class MyFileLogger: public libfreenect2::Logger{
-	private:
-		std::ofstream logfile_;
-	public:
-		MyFileLogger(const char *filename){
-    	if (filename)
-			logfile_.open(filename);
-    	level_ = Debug;
-  }
-  bool good(){
-    return logfile_.is_open() && logfile_.good();
-  }
-  virtual void log(Level level, const std::string &message){
-    logfile_ << "[" << libfreenect2::Logger::level2str(level) << "] " << message << std::endl;
-  }
-};
+#include "myFileLogger.cpp"
 
 int main(int argc, char* argv[]){
 	//Inicia a libfreenect2--------------------------------------------------
